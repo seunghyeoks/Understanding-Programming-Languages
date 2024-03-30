3c34a14eab394076a64f7ae67e70c13999e62e8d
BookShelf <seunghyeoks@icloud.com>
2024-03-30 18:55:38 +0900

    fix: limit test 3 times

--- Week04/hw4/hw4_test.ml
+++ Week04/hw4/hw4_test.ml
@@ -3,7 +3,7 @@ let%test _ = Hw4.lex "1 * 361 - 89 + 2 / 56" = [Hw4.TI 1; Hw4.TO '*'; Hw4.TI 361
 let%test _ = Hw4.lex "0 * 1   2 - 0   23 /" = [Hw4.TI 0; Hw4.TO '*'; Hw4.TI 12; Hw4.TO '-'; Hw4.TI 23; Hw4.TO '/']
 let%test _ = try Hw4.lex "3 3 2 * * 2" = [] with Failure msg -> msg = "Failed in Lexing"
 
-(* additional test *)
+(* additional test 
 let%test _ = Hw4.lex "1 +  " = [Hw4.TI 1; Hw4.TO '+']
 let%test _ = Hw4.lex "1   2" = [Hw4.TI 12]
 
@@ -14,3 +14,4 @@ let%test _ = try Hw4.lex "3 3 2 * / 2" = [] with Failure msg -> msg = "Failed in
 let%test _ = try Hw4.lex "+23 - 1" = [] with Failure msg -> msg = "Failed in Lexing"
 let%test _ = try Hw4.lex "1 + 2.0" = [] with Failure msg -> msg = "Failed in Lexing"
 let%test _ = try Hw4.lex "1 % 2" = [] with Failure msg -> msg = "Failed in Lexing"
+*)
