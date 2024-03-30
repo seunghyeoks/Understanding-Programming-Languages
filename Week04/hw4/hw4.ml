e43db04c393a874602e82de862f236ec6ff2f400
BookShelf <seunghyeoks@icloud.com>
2024-03-30 13:34:17 +0900

    add List.filter in toCharList

--- Week04/hw4/hw4.ml
+++ Week04/hw4/hw4.ml
@@ -2,11 +2,14 @@ type token = TI of int | TO of char
 
 let toCharList (s : string) : char list = 
   let s = String.to_seq s in 
-  let s = List.of_seq s in
+  let s = List.of_seq s in 
+  let s = List.filter (fun x -> (x != ' ')) s in
   s
+
+
 
 let lex (s : string) : token list = 
   let buffer1 : char list = toCharList s in
   let buffer2 : token list = [] in 
-  let _ = List.iter (Format.printf "%c ") buffer1 in
-  buffer2
+  let _ = List.iter (fun x -> buffer2 @ [x]) buffer1 in 
+  let _ = List.iter (Format.printf "%c ") buffer2
