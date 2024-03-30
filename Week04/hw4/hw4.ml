a1620d9b5d6160fbdd958ea8b8bdca42ddb8c1d2
BookShelf <seunghyeoks@icloud.com>
2024-03-30 15:48:53 +0900

    fix lex

--- Week04/hw4/hw4.ml
+++ Week04/hw4/hw4.ml
@@ -6,7 +6,7 @@ let toCharList (s : string) : char list =
   let s = List.filter (fun x -> (x != ' ')) s in
   s
 
-let toIntToken (lst : char list) : token =
+let listToTI (lst : char list) : token =
   let lst = List.to_seq lst in
   let lst = String.of_seq lst in
   let lst = int_of_string lst in
@@ -17,13 +17,15 @@ let lex (s : string) : token list =
   let charlst : char list = toCharList s in
   let buffer1 : char list = [] in
   let buffer2 : token list = [] in 
-  let _ = List.iter (fun x -> 
-            match x with
-            | ('0' .. '9') -> buffer1 @ [x]
-            | '+' -> begin buffer2 = buffer2 @ [toIntToken buffer1] ; buffer1 = [] ; buffer2 = buffer2 @ [x] end
-            | '-' -> begin buffer2 = buffer2 @ [toIntToken buffer1] ; buffer1 = [] ; buffer2 = buffer2 @ [x] end
-            | '*' -> begin buffer2 = buffer2 @ [toIntToken buffer1] ; buffer1 = [] ; buffer2 = buffer2 @ [x] end
-            | '/' -> begin buffer2 = buffer2 @ [toIntToken buffer1] ; buffer1 = [] ; buffer2 = buffer2 @ [x] end
-            | _   -> failwith "Failed in Lexing"
-          ) charlst in 
-  let _ = List.iter (Format.printf "%a ") buffer2
+  let rec lex' lst b1 b2 = 
+    match lst with
+    | [] -> b2
+    | a :: t -> 
+      match a with
+      | '0' .. '9' -> lex' t (b1 @ [a]) b2
+      | '+' | '-' | '*' | '/' ->
+        let b2_ = b2 @ [listToTI b1] in
+        let b1_ = [] in
+        lex' t b1_ (b2_ @ [TO a])
+      | _ -> failwith "Failed with Lexing"
+  in lex' charlst buffer1 buffer2
