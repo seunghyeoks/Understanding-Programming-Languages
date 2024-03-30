4f8984fe0ac52975aa46d395bb105a861a96773f
BookShelf <seunghyeoks@icloud.com>
2024-03-30 15:00:50 +0900

    fix toIntToken

--- Week04/hw4/hw4.ml
+++ Week04/hw4/hw4.ml
@@ -1,6 +1,5 @@
 type token = TI of int | TO of char
 
-
 let toCharList (s : string) : char list = 
   let s = String.to_seq s in 
   let s = List.of_seq s in 
@@ -11,19 +10,19 @@ let toIntToken (lst : char list) : token =
   let lst = List.to_seq lst in
   let lst = String.of_seq lst in
   let lst = int_of_string lst in
-  TO lst
+  TI lst
 
 
 let lex (s : string) : token list = 
   let charlst : char list = toCharList s  in
   let buffer1 : char list = [] in
   let buffer2 : token list = [] in 
-    let _ = List.iter (fun x : token list-> 
-              match x with
-              | ('0' .. '9') -> buffer1 @ [x]
-              | '+' -> buffer2 @ [buffer1]
-              | '-' -> buffer2 @ [TO x]
-              | '*' -> buffer2 @ [TO x]
-              | '/' -> buffer2 @ [TO x]
-              | _   -> failwith "Failed in Lexing"
-            ) charlst in !buffer2
+  let _ = List.iter (fun x -> 
+      match x with
+      | ('0' .. '9') -> buffer1 @ [x]
+      | '+' -> let _ = buffer2 @ [toIntToken buffer1] in let buffer1 = [] in buffer2 @ x
+      | '-' -> let _ = buffer2 @ [toIntToken buffer1] in let buffer1 = [] in buffer2 @ x
+      | '*' -> let _ = buffer2 @ [toIntToken buffer1] in let buffer1 = [] in buffer2 @ x
+      | '/' -> let _ = buffer2 @ [toIntToken buffer1] in let buffer1 = [] in buffer2 @ x
+      | _   -> failwith "Failed in Lexing"
+    ) charlst 
