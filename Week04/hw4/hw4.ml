302ee4dde292a035cfe0987aff62d857fcb965a0
BookShelf <seunghyeoks@icloud.com>
2024-03-30 18:54:54 +0900

    add: hw4_test

--- Week04/hw4/hw4.ml
+++ Week04/hw4/hw4.ml
@@ -1,6 +1,8 @@
 type token = TI of int | TO of char
 type state = Q0 | Q1 | Q2
 
+
+
 (* String에서 공백을 제외한 글자를 하나씩 뽑아 char list로 반환하는 함수 *)
 let toCharList (s : string) : char list = 
   let s = String.to_seq s in 
@@ -8,44 +10,46 @@ let toCharList (s : string) : char list =
   let s = List.filter (fun x -> (x != ' ')) s in
   s
 
-(* 숫자 char만 들어있는 char list를 TI(int)로 변환하는 함수 *)
-let listToToken (lst : char list) : token =
-  match lst with
+
+
+(* char list의 맨 앞 요소를 확인하여 알맞은 토큰으로 변환하는 함수 *)
+let listToToken (b1 : char list) : token =
+  match b1 with
   | [] -> failwith "Failed in Lexing"
   | a :: _ -> 
     match a with
-    | '+' | '-' | '*' | '/' -> TO a
-    | _ ->  let lst = List.to_seq lst in
-            let lst = String.of_seq lst in
-            let lst = int_of_string lst in
-            TI lst
+    | '+' | '-' | '*' | '/' -> TO a         (* TO 토큰의 경우 *)
+    | _ ->  let b1 = List.to_seq b1 in      (* TI 토큰의 경우 *)
+            let b1 = String.of_seq b1 in
+            let b1 = int_of_string b1 in
+            TI b1
 
 
 
-(* 재귀로 구현 *)
+(* 재귀와 삼중 타입 매칭으로 구현 *)
 let lex (s : string) : token list =
   let rec lex' lst q b1 b2 = 
     match q with 
     | Q0 -> 
       (match lst with
-      | [] -> failwith "Failed in Lexing"
+      | [] -> failwith "Failed in Lexing"         (*  fail at q0  *)
       | a :: t -> 
         (match a with
-        | '0' .. '9' -> lex' t Q1 (b1 @ [a]) b2  
-        | _ -> failwith "Failed in Lexing"))
+        | '0' .. '9' -> lex' t Q1 (b1 @ [a]) b2   (*  (q0,[0-9]) -> q1   *)
+        | _ -> failwith "Failed in Lexing"))      (*  fail at q0  *)
     | Q1 -> 
       (match lst with
-      | [] -> b2 @ [listToToken b1]
+      | [] -> b2 @ [listToToken b1]               (*  final at q1 *)
       | a :: t -> 
         (match a with
-        | '0' .. '9' -> lex' t Q1 (b1 @ [a]) b2  
-        | '+' | '-' | '*' | '/' -> lex' t Q2 [a] (b2 @ [listToToken b1])
-        | _ -> failwith "Failed in Lexing"))
+        | '0' .. '9' -> lex' t Q1 (b1 @ [a]) b2   (*  (q1,[0-9]) -> q1  *)
+        | '+' | '-' | '*' | '/' -> lex' t Q2 [a] (b2 @ [listToToken b1])  (*  (q1,[+|-|*|/]) -> q2  *)
+        | _ -> failwith "Failed in Lexing"))      (*  fail at q1  *)
     | Q2 -> 
       (match lst with
-      | [] -> b2 @ [listToToken b1]
+      | [] -> b2 @ [listToToken b1]               (*  final at q2  *)
       | a :: t -> 
         (match a with
-        | '0' .. '9' -> lex' t Q1 [a] (b2 @ [listToToken b1])
-        | _ -> failwith "Failed in Lexing"))
+        | '0' .. '9' -> lex' t Q1 [a] (b2 @ [listToToken b1]) (*  (q2,[0-9]) -> q1  *)
+        | _ -> failwith "Failed in Lexing"))      (*  fail at q2  *)
   in lex' (toCharList s) Q0 [] []
--- Week04/hw4/hw4_test.ml
+++ Week04/hw4/hw4_test.ml
@@ -1,13 +1,16 @@
 (* lex *)
-let%test _ = Hw4.lex "1 + 2" = [Hw4.TI 1; Hw4.TO '+'; Hw4.TI 2]
+let%test _ = Hw4.lex "1 * 361 - 89 + 2 / 56" = [Hw4.TI 1; Hw4.TO '*'; Hw4.TI 361; Hw4.TO '-'; Hw4.TI 89; Hw4.TO '+'; Hw4.TI 2; Hw4.TO '/'; Hw4.TI 56]
+let%test _ = Hw4.lex "0 * 1   2 - 0   23 /" = [Hw4.TI 0; Hw4.TO '*'; Hw4.TI 12; Hw4.TO '-'; Hw4.TI 23; Hw4.TO '/']
+let%test _ = try Hw4.lex "3 3 2 * * 2" = [] with Failure msg -> msg = "Failed in Lexing"
+
+(* additional test *)
 let%test _ = Hw4.lex "1 +  " = [Hw4.TI 1; Hw4.TO '+']
 let%test _ = Hw4.lex "1   2" = [Hw4.TI 12]
 
-let%test _ = Hw4.lex "1 * 361 - 89 + 2 / 56" = [Hw4.TI 1; Hw4.TO '*'; Hw4.TI 361; Hw4.TO '-'; Hw4.TI 89; Hw4.TO '+'; Hw4.TI 2; Hw4.TO '/'; Hw4.TI 56]
 let%test _ = Hw4.lex "1 * 361 - 89 + 2 /" = [Hw4.TI 1; Hw4.TO '*'; Hw4.TI 361; Hw4.TO '-'; Hw4.TI 89; Hw4.TO '+'; Hw4.TI 2; Hw4.TO '/']
 
 let%test _ = try Hw4.lex "1 ++ 2" = [] with Failure msg -> msg = "Failed in Lexing"
-let%test _ = try Hw4.lex "3 3 2 * * 2" = [] with Failure msg -> msg = "Failed in Lexing"
-let%test _ = try Hw4.lex "+ /23" = [] with Failure msg -> msg = "Failed in Lexing"
+let%test _ = try Hw4.lex "3 3 2 * / 2" = [] with Failure msg -> msg = "Failed in Lexing"
+let%test _ = try Hw4.lex "+23 - 1" = [] with Failure msg -> msg = "Failed in Lexing"
 let%test _ = try Hw4.lex "1 + 2.0" = [] with Failure msg -> msg = "Failed in Lexing"
 let%test _ = try Hw4.lex "1 % 2" = [] with Failure msg -> msg = "Failed in Lexing"
