8379578183189ff1b61d37bc3af7ca80d0bbc8de
BookShelf <seunghyeoks@icloud.com>
2024-05-10 12:35:10 +0900

    test: add testcase for [Week10]

--- Assignment/Week10/hw10.ml
+++ Assignment/Week10/hw10.ml
@@ -4,6 +4,7 @@ let rec interp (e : Ast.expr) (t : Store.t) : Store.value =
   match e with
   | Ast.Num (n) -> Store.NumV n
   | Ast.Id (x) -> Store.find x t
+
   | Ast.Add (e1, e2) -> 
     let n1 = interp e1 t in
     let n2 = interp e2 t in
@@ -12,6 +13,7 @@ let rec interp (e : Ast.expr) (t : Store.t) : Store.value =
       | NumV n1, NumV n2 -> NumV (n1 + n2)
       | _ -> failwith (Format.asprintf "[Error] Not a number: %a + %a" Ast.pp e1 Ast.pp e2)
     end
+    
   | Ast.Sub (e1, e2) -> 
     let n1 = interp e1 t in
     let n2 = interp e2 t in
@@ -20,10 +22,12 @@ let rec interp (e : Ast.expr) (t : Store.t) : Store.value =
       | NumV n1, NumV n2 -> NumV (n1 - n2)
       | _ -> failwith (Format.asprintf "[Error] Not a number: %a - %a" Ast.pp e1 Ast.pp e2)
     end
+
   | Ast.LetIn (x, e1, e2) -> 
     let v1 = interp e1 t in
     let v2 = interp e2 (Store.add x v1 t) in
     v2
+
   | Ast.Lambda (x, e) -> ClosureV (x, e, t)
   | Ast.App (e1, e2) ->
     let f1 = interp e1 t in
--- Assignment/Week10/hw10_test.ml
+++ Assignment/Week10/hw10_test.ml
@@ -1,2 +1,17 @@
 let%test _ = Hw10.interp (ParserMain.parse "(fun x y -> x + y) 1 3") Store.empty = Store.NumV 4
 let%test _ = Hw10.interp (ParserMain.parse "let f = (fun x y -> x + y) in f 1 3") Store.empty = Store.NumV 4
+let%test _ = Hw10.interp (ParserMain.parse "let foo = (fun x -> (fun y -> y + x)) in foo 2 3") Store.empty = Store.NumV 5
+
+(* additional test 
+let%test _ = Hw10.interp (ParserMain.parse "let foo = (fun x -> x + 1) in let bar = (fun x -> x 1) in (bar foo)") Store.empty = Store.NumV 2
+let%test _ = Hw10.interp (ParserMain.parse "let foo = (fun x -> x + 1) in let bar = (fun x -> foo x) in (foo (bar 3))") Store.empty = Store.NumV 5
+let%test _ = Hw10.interp (ParserMain.parse "let x = 0 in let f = (fun y -> y + x) in let x = 99 in f 1") Store.empty = Store.NumV 1
+let%test _ = Hw10.interp (ParserMain.parse "let y = 2 in let f = (fun x -> x + y) in let y = 3 in (f y)") Store.empty = Store.NumV 5
+
+let%test _ = try Hw10.interp (ParserMain.parse "let f = (fun x -> x + 1) in f + 1") Store.empty = Store.NumV 0 with Failure msg -> msg = "[Error] Not a number: f + 1"
+let%test _ = try Hw10.interp (ParserMain.parse "(fun x -> x + 1) - 1") Store.empty = Store.NumV 0 with Failure msg -> msg = "[Error] Not a number: (fun x -> x + 1) - 1"
+let%test _ = try Hw10.interp (ParserMain.parse "1 1") Store.empty = Store.NumV 0 with Failure msg -> msg = "[Error] Not a function: 1"
+let%test _ = try Hw10.interp (ParserMain.parse "v 1") Store.empty = Store.NumV 0 with Failure msg -> msg = "[Error] Free identifier: v"
+let%test _ = try Hw10.interp (ParserMain.parse "let v = 1 in v 1") Store.empty = Store.NumV 0 with Failure msg -> msg = "[Error] Not a function: let v = 1 in v"
+let%test _ = try Hw10.interp (ParserMain.parse "let v = 1 in (v 1)") Store.empty = Store.NumV 0 with Failure msg -> msg = "[Error] Not a function: v"
+*)
