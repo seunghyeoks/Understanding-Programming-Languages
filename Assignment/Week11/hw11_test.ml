286c1e2227e10c971f250c847e9fd88b4334f6fa
BookShelf <seunghyeoks@icloud.com>
2024-05-17 11:31:54 +0900

    feat: add [Week11]

---
+++ Assignment/Week11/hw11_test.ml
@@ -0,0 +1,18 @@
+let%test _ = Hw11.interp (ParserMain.parse "if 0 < 1 then 2 else 3") Store.empty = Store.ClosureV ("y", Ast.Id "x", [("x", Store.NumV 3); ("true", Store.NumV 2)])
+let%test _ = Hw11.interp (ParserMain.parse "if true then 2 else 3") Store.empty = Store.NumV 2
+let%test _ = Hw11.interp (ParserMain.parse "if false then 0 else 1") Store.empty = Store.NumV 1
+
+
+(* additional test 
+let%test _ = Hw10.interp (ParserMain.parse "let foo = (fun x -> x + 1) in let bar = (fun x -> x 1) in (bar foo)") Store.empty = Store.NumV 2
+let%test _ = Hw10.interp (ParserMain.parse "let foo = (fun x -> x + 1) in let bar = (fun x -> foo x) in (foo (bar 3))") Store.empty = Store.NumV 5
+let%test _ = Hw10.interp (ParserMain.parse "let x = 0 in let f = (fun y -> y + x) in let x = 99 in f 1") Store.empty = Store.NumV 1
+let%test _ = Hw10.interp (ParserMain.parse "let y = 2 in let f = (fun x -> x + y) in let y = 3 in (f y)") Store.empty = Store.NumV 5
+
+let%test _ = try Hw10.interp (ParserMain.parse "let f = (fun x -> x + 1) in f + 1") Store.empty = Store.NumV 0 with Failure msg -> msg = "[Error] Not a number: f + 1"
+let%test _ = try Hw10.interp (ParserMain.parse "(fun x -> x + 1) - 1") Store.empty = Store.NumV 0 with Failure msg -> msg = "[Error] Not a number: (fun x -> x + 1) - 1"
+let%test _ = try Hw10.interp (ParserMain.parse "1 1") Store.empty = Store.NumV 0 with Failure msg -> msg = "[Error] Not a function: 1"
+let%test _ = try Hw10.interp (ParserMain.parse "v 1") Store.empty = Store.NumV 0 with Failure msg -> msg = "[Error] Free identifier: v"
+let%test _ = try Hw10.interp (ParserMain.parse "let v = 1 in v 1") Store.empty = Store.NumV 0 with Failure msg -> msg = "[Error] Not a function: let v = 1 in v"
+let%test _ = try Hw10.interp (ParserMain.parse "let v = 1 in (v 1)") Store.empty = Store.NumV 0 with Failure msg -> msg = "[Error] Not a function: v"
+*)
---
+++ Assignment/Week11/store.ml
@@ -0,0 +1,29 @@
+(* 추상메모리에 대한 구현 *)
+
+type t = (string * value) list
+and value = NumV of int
+  | ClosureV of string * Ast.expr * t
+
+let empty : t = [] 
+
+(* 추상메모리 t에서 변수 s에 맞는 값을 반환*)
+let rec find (s : string) (t : t) : value = 
+  match t with
+  | [] -> failwith ("[Error] Free identifier: " ^ s)
+  | fst :: remain -> 
+    (match fst with
+    | (var, num) -> 
+      if var = s then num
+      else find s remain
+    )
+
+(* 추상메모리 t에서 변수 s의 값을 v로 업데이트한 새로운 메모리를 반환 *)
+(* 기존 t에 이미 s가 있을 경우, 그것을 제거하고 새로운 (s, v)를 추가 *)
+let add (s : string) (v : value) (t : t) : t = 
+  let t = List.filter (fun x -> 
+    match x with
+    | (var, _) -> 
+      if var = s then false
+      else true
+    ) t 
+  in (s, v) :: t
